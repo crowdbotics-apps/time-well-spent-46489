75f35cc4353e6af5c12f143a5a519cddcbe0bd7f
Crowdbotics <team@crowdbotics.com>
2024-02-20 10:13:13 +0000

    Initial commit

---
+++ backend/time_well_spent_46489/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for time_well_spent_46489 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "time_well_spent_46489.settings"
+)
+
+application = get_wsgi_application()
